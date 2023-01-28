ccb7aaf381aada8b74504effdcf24a779a76dcf3
zhangdianheng <dream_zdh@163.com>
2023-01-28 16:36:49 +0800

    # proto实例

--- java-zdh/src/main/java/com/zdh/protobuf/DemoProto.java
+++ java-zdh/src/main/java/com/zdh/protobuf/DemoProto.java
@@ -6,4 +6,15 @@ package com.zdh.protobuf;
  * @Version 1.0
  */
 public class DemoProto {
+
+    public static void main(String[] args) {
+        StudentProto.Student zdh = StudentProto.Student.newBuilder()
+                .setAge(12)
+                .setName("zdh")
+                .build();
+        System.out.println(zdh);
+        byte[] bytes = zdh.toByteArray();
+
+
+    }
 }
